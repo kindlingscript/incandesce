36107b592fcbf7d82765d8199946b2e64a6f4d94
Rebecca Taylor <inflamedmuse@gmail.com>
2015-11-10 03:01:30 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Incandesce::Application.config.secret_key_base = '91279f386bd34a77e7f347fd8f0b1ea85e4403138d780310ea5d64ae4386525fbf9c70ef6ced6debe7acbf015d7739ce00aaeed2859ec5e55fd122680e5a3d81'
