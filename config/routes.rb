ece17ede24650efe195df8d13747e75720201cbb
Rebecca Taylor <inflamedmuse@gmail.com>
2015-11-14 05:45:25 +0000

    Built up add lesson functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Incandesce::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  resources :courses, :only => [:index, :show]
   namespace :instructor do
+    resources :sections, :only => [] do
+      resources :lessons, :only => [:new, :create]
+    end
     resources :courses, :only => [:new, :create, :show] do
       resources :sections, :only => [:new, :create]
     end
   end
-  resources :courses, :only => [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
